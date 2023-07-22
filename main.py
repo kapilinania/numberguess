1b2de8dbf428c27808a2ae00c0533f1b2fbba8ef
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-22 21:30:39 +0530

    done

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# here i am doing number guess
+import random
+# first  I am import random
+
+
+num = random.randrange(1, 20)
+
+while True:
+    userenter = int(input("Guess a Number: "))
+
+    if userenter > num:
+        print("Very high number")
+    elif userenter < num:
+        print("Very low number")
+    else:
+        print("You guessed the right number!")
+        break
